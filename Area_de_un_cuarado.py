ca223ba93810f929a248645b5921aef0d0f1354f
pythongabriel12 <137719552+pythongabriel12@users.noreply.github.com>
2023-06-25 11:56:58 -0600

    Create Area_de_un_cuarado.py
    Programa para sacar area de un cuadrado en python..

---
+++ Area_de_un_cuarado.py
@@ -0,0 +1,20 @@
+#Programa para calcular el area en un cuadrado en python
+#El simbolo (*) significa multiplicacion en python
+
+#creamos dos variabes para cada lado con input para preguntar
+#y float para valores con punto decimal
+
+lado1 = float(input('Dame el lado 1:'))
+lado2 = float(input('Dame el lado 2:'))
+
+#Creamos variable para hacer la formula :)
+#dentro de la variable multiplicamos el lado1 por el lado 2
+area = lado1 * lado2
+
+#Por ultimo mostramos en pantalla el resultado con un print y dentro la vareable
+#area
+
+print(area)
+#corremos el programa
+
+# y listo eso es todo espero les haya gustado..
